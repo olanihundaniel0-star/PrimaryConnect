515ba4acd14495cbcc8776aa7e2707750ed811da
Olanihundaniel0-star <Olanihundaniel0@gmail.com>
2026-07-19 19:43:55 +0100

    Add fields to Pupil, Score, AttendanceRecord, and Exercise models

--- src/main/java/com/primaryconnect/model/Pupil.java
+++ src/main/java/com/primaryconnect/model/Pupil.java
@@ -4,6 +4,50 @@ package com.primaryconnect.model;
  * Represents a pupil record stored by the system.
  */
 public class Pupil {
+    private int pupilId;
+    private String name;
+    private String classLevel;
+    private String guardianContact;
+
     public Pupil() {
+    }
+
+    public Pupil(int pupilId, String name, String classLevel, String guardianContact) {
+        this.pupilId = pupilId;
+        this.name = name;
+        this.classLevel = classLevel;
+        this.guardianContact = guardianContact;
+    }
+
+    public int getPupilId() {
+        return pupilId;
+    }
+
+    public void setPupilId(int pupilId) {
+        this.pupilId = pupilId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClassLevel() {
+        return classLevel;
+    }
+
+    public void setClassLevel(String classLevel) {
+        this.classLevel = classLevel;
+    }
+
+    public String getGuardianContact() {
+        return guardianContact;
+    }
+
+    public void setGuardianContact(String guardianContact) {
+        this.guardianContact = guardianContact;
     }
 }
